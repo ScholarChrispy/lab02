5a313d220ce331f3a57b167fa19514de4ab4ccba
ScholarChrispy <90281095+ScholarChrispy@users.noreply.github.com>
2022-02-09 21:24:46 -0500

    Initial commit. Added SimpleTest.java

---
+++ src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,6 @@
+// --- Christopher Johnson | chrispy1002@gmail.com | 2022-Feb-09 | SimpleTest.java --- //
+
+// main class (simple hello world)
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+}
